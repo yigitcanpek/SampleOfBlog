35c3133bf4ce1be85c0378e8d1ad7f9c8791357d
Yiğit Can Pekgüzel <yigitcanpekguzel@gmail.com>
2021-11-10 03:13:51 +0300

    Authentication class'ı yazildi ve Authorize attirbute olarak admin controllerına eklendi. Intiliaze ayarları düzeltildi. User Login arayüzü oluşturuldu

---
+++ Project.MVCUI/Controllers/PanelController.cs
@@ -0,0 +1,19 @@
+﻿using Project.MVCUI.AuthenticationClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.MVCUI.Controllers
+{
+    public class PanelController : Controller
+    {
+        // GET: Panel
+        [AdminAuthentication]
+        public ActionResult AdminPanel()
+        {
+            return View();
+        }
+    }
+}
