04646722c480968add78fba95ba41f53fe69fe39
Kadir <kadirsofor@hotmail.com>
2021-11-19 19:37:18 +0300

    Widgetların bitişi, introduce section ve headerda düzeltmeler, javascript kodlarının optimizasyonu.

---
+++ Project.MVCUI/Scripts/Js/script.js
@@ -0,0 +1,42 @@
+﻿latestChild = document.querySelectorAll('div.latest-widget > ul > li > a, div.categories-widget > ul > li > a');
+
+function mouseOver() {
+    event.target.classList.add("link-hover");
+}
+function mouseOut() {
+    event.target.classList.remove("link-hover");
+}
+for (i = 0; i < latestChild.length; i++) {
+    latestChild[i].addEventListener("mouseover", mouseOver);
+    latestChild[i].addEventListener("mouseout", mouseOut);
+}
+
+/*
+latestParent = document.querySelectorAll('div.latest-widget > ul > li ');
+latestChild = document.querySelectorAll('div.latest-widget > ul > li > a');
+
+categoryParent = document.querySelectorAll('div.categories-widget > ul > li ');
+categoryChild = document.querySelectorAll('div.categories-widget > ul > li > a');
+
+function mouseOver() {
+    event.target.classList.add("link-hover");
+}
+function mouseOut() {
+    event.target.classList.remove("link-hover");
+}
+
+
+for (i = 0; i < latestParent.length + categoryParent.length; i++) {
+    latestChild[i].addEventListener("mouseover", mouseOver);
+    latestChild[i].addEventListener("mouseout", mouseOut);
+
+    categoryChild[i].addEventListener("mouseover", mouseOver);
+    categoryChild[i].addEventListener("mouseout", mouseOut);
+
+    if (i != latestParent.length - 1 && i != categoryParent.length - 1) {
+        latestParent[i].style.borderBottom = "1px solid #e6e6e6";
+        categoryParent[i].style.borderBottom = "1px solid #e6e6e6";
+    }
+
+}
+*/
