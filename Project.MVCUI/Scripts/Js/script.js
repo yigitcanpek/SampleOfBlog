45bc19a46b1178e3342310731bb3380b83dbe5b2
Kadir <kadirsofor@hotmail.com>
2021-11-21 16:10:03 +0300

    Footer ve post akış kısmının büyük kısmı bitti, ufak stil düzeltmeleri ve stil dosyasının optimizasyonu yapılacak.

--- Project.MVCUI/Scripts/Js/script.js
+++ Project.MVCUI/Scripts/Js/script.js
@@ -1,4 +1,4 @@
-﻿latestChild = document.querySelectorAll('div.latest-widget > ul > li > a, div.categories-widget > ul > li > a');
+﻿latestChild = document.querySelectorAll('div.latest-widget > ul > li > a, div.categories-widget > ul > li > a, .post-title > a');
 
 function mouseOver() {
     event.target.classList.add("link-hover");
@@ -10,6 +10,28 @@ for (i = 0; i < latestChild.length; i++) {
     latestChild[i].addEventListener("mouseover", mouseOver);
     latestChild[i].addEventListener("mouseout", mouseOut);
 }
+
+
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.left >= 0 
+    );
+}
+
+
+const box = document.getElementById('myfooter');
+const btn = document.getElementById('go-top');
+
+document.addEventListener('scroll', function () {
+    const bgColor = isInViewport(box) ?
+        'hidden' :
+        'visible';
+    btn.style.visibility = bgColor;
+}, {
+    passive: true
+});
 
 /*
 latestParent = document.querySelectorAll('div.latest-widget > ul > li ');
